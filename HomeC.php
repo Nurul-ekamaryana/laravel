a6d73cb4757a8b858addf1145cfe5b4040d5885a
NURUL EKA MARYANA <110007326+Nurul-ekamaryana@users.noreply.github.com>
2022-12-01 19:09:55 +0700

    Add files via upload

--- HomeC.php
+++ HomeC.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\StudentsM;
+use App\Models\dataprestasiM;
+use App\Models\eskulM;
+use App\Models\pesertadidikM;
+use App\Models\about;
+
 
 class HomeC extends Controller
 {
     public function index(){
-        $user = ['name' => 'Dazai', 'role' => 'teller'];
-    return view('pages.home', $user);
+        return view('pages.home');
     }
-
+    
+    public function pesertadidik(){
+        $data = pesertadidikM::all();
+        return view('pages.pesertadidik', ['data' => $data]);
+    }
+    public function eskul (){
+        $data = eskulM::all();
+        return view('pages.eskul', ['data' => $data]);
+    }
+    public function dataprestasi (){
+        $data = dataprestasiM::all();
+        return view('pages.dataprestasi', ['data' => $data]);
+    }
     public function about(){
-        $user = ['name' => 'chuya', 'role' => 'admin'];
-        return view('pages.about', $user);
+        return view('pages.about');
     }
-
-    public function contact(){
-        $data = StudentsM::all();
-        return view('pages.contact', ['data' => $data]);
-    }
+    
 }
